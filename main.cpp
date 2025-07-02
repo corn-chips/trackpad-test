05bedac3a5b59af88b0ddac95990f397369ac064
Patrick Tsang <pattsa240@hotmail.com>
2025-07-01 22:42:23 -0700

    w

--- main.cpp
+++ main.cpp
@@ -3,13 +3,13 @@
 #include <map>
 
 // Global map to store finger positions.
-// Key: Finger ID (DWORD), Value: Position (POINT)
-std::map<DWORD, POINT> g_touchPoints;
+// Key: Pointer ID (UINT32), Value: Position (POINT)
+std::map<UINT32, POINT> g_touchPoints;
 
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow) {
-    const wchar_t CLASS_NAME[] = L"TouchSampleWindowClass";
+    const wchar_t CLASS_NAME[] = L"PointerSampleWindowClass";
 
     WNDCLASS wc = {};
     wc.lpfnWndProc = WndProc;
@@ -21,7 +21,7 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow
     RegisterClass(&wc);
 
     HWND hwnd = CreateWindowEx(
-        0, CLASS_NAME, L"Win32 Trackpad Touch Example",
+        0, CLASS_NAME, L"Win32 Pointer (Multi-Touch) Example",
         WS_OVERLAPPEDWINDOW,
         CW_USEDEFAULT, CW_USEDEFAULT, 800, 600,
         NULL, NULL, hInstance, NULL
@@ -31,10 +31,7 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow
         return 0;
     }
 
-    // Register the window for touch input
-    if (!RegisterTouchWindow(hwnd, 0)) {
-        MessageBox(hwnd, L"Error: Could not register for touch input.", L"Error", MB_OK | MB_ICONERROR);
-    }
+    // No registration required for WM_POINTER messages!
 
     ShowWindow(hwnd, nCmdShow);
 
@@ -47,87 +44,75 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow
     return 0;
 }
 
-void ProcessTouchInput(HWND hwnd, UINT numInputs, HTOUCHINPUT hTouchInput) {
-    TOUCHINPUT* pInputs = new TOUCHINPUT[numInputs];
-    if (pInputs == NULL) {
-        return;
-    }
+LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) {
+    // Extract the pointer ID from a pointer message
+    UINT32 pointerId = GET_POINTERID_WPARAM(wParam);
+    POINTER_INFO pointerInfo = {};
 
-    if (GetTouchInputInfo(hTouchInput, numInputs, pInputs, sizeof(TOUCHINPUT))) {
-        for (UINT i = 0; i < numInputs; i++) {
-            TOUCHINPUT ti = pInputs[i];
-
-            POINT pt;
-            pt.x = TOUCH_COORD_TO_PIXEL(ti.x);
-            pt.y = TOUCH_COORD_TO_PIXEL(ti.y);
-            ScreenToClient(hwnd, &pt);
-
-            if (ti.dwFlags & TOUCHEVENTF_DOWN) {
-                // Finger went down, add it to our map
-                g_touchPoints[ti.dwID] = pt;
-            }
-            else if (ti.dwFlags & TOUCHEVENTF_MOVE) {
-                // Finger is moving, update its position
-                g_touchPoints[ti.dwID] = pt;
-            }
-            else if (ti.dwFlags & TOUCHEVENTF_UP) {
-                // Finger went up, remove it from our map
-                g_touchPoints.erase(ti.dwID);
+    switch (message) {
+    case WM_POINTERDOWN:
+    {
+        // Get the full information for this pointer
+        if (GetPointerInfo(pointerId, &pointerInfo)) {
+            // We are only interested in touch inputs
+            if (pointerInfo.pointerType == PT_TOUCH) {
+                POINT pt = pointerInfo.ptPixelLocation;
+                ScreenToClient(hwnd, &pt);
+                // Add the new finger to our map
+                g_touchPoints[pointerId] = pt;
+                InvalidateRect(hwnd, NULL, TRUE);
             }
         }
+        return 0; // Mark message as handled
     }
 
-    // CRITICAL: Must close the handle to prevent resource leaks
-    CloseTouchInputHandle(hTouchInput);
-    delete[] pInputs;
+    case WM_POINTERUPDATE:
+    {
+        if (GetPointerInfo(pointerId, &pointerInfo)) {
+            if (pointerInfo.pointerType == PT_TOUCH) {
+                POINT pt = pointerInfo.ptPixelLocation;
+                ScreenToClient(hwnd, &pt);
+                // Update the finger's position in our map
+                g_touchPoints[pointerId] = pt;
+                InvalidateRect(hwnd, NULL, TRUE);
+            }
+        }
+        return 0; // Mark message as handled
+    }
 
-    // Force the window to be redrawn
-    InvalidateRect(hwnd, NULL, TRUE);
-}
+    case WM_POINTERUP:
+    {
+        // This pointer is now 'up', so remove it from our map
+        g_touchPoints.erase(pointerId);
+        InvalidateRect(hwnd, NULL, TRUE);
+        return 0; // Mark message as handled
+    }
 
-LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) {
-    switch (message) {
     case WM_PAINT:
     {
         PAINTSTRUCT ps;
         HDC hdc = BeginPaint(hwnd, &ps);
-
-        // White background
         FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
 
-        // Create a brush to draw the circles
-        HBRUSH hBrush = CreateSolidBrush(RGB(0, 0, 200)); // Blue
+        HBRUSH hBrush = CreateSolidBrush(RGB(200, 0, 0)); // Red
         HBRUSH hOldBrush = (HBRUSH)SelectObject(hdc, hBrush);
 
         // Draw a circle for each currently tracked finger
         for (auto const& [id, pt] : g_touchPoints) {
-            // Draw a circle of radius 20 around the touch point
             Ellipse(hdc, pt.x - 20, pt.y - 20, pt.x + 20, pt.y + 20);
         }
 
         SelectObject(hdc, hOldBrush);
         DeleteObject(hBrush);
-
         EndPaint(hwnd, &ps);
     }
     return 0;
-
-    case WM_TOUCH:
-    {
-        UINT numInputs = LOWORD(wParam);
-        HTOUCHINPUT hTouchInput = (HTOUCHINPUT)lParam;
-        ProcessTouchInput(hwnd, numInputs, hTouchInput);
-        return 0;
-    }
 
     case WM_CLOSE:
         DestroyWindow(hwnd);
         break;
 
     case WM_DESTROY:
-        // Unregister is good practice, though not strictly required as
-        // window destruction cleans up registrations.
-        UnregisterTouchWindow(hwnd);
         PostQuitMessage(0);
         break;
 
